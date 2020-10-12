b17dd95677590801eb72974a5c5b5b633a5db697
Mohit Hooda <mhooda87@gmail.com>
2020-10-12 23:23:57 +0530

    Hello changes

--- manage.py
+++ manage.py
@@ -9,8 +9,6 @@ if __name__ == "__main__":
     except ImportError as exc:
         raise ImportError(
             print("Ther is an error")
-            print("Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?")
+            print("Couldn't import Django. Are you sure it's installed and available on your PYTHONPATH environment variable? Did you forget to activate a virtual environment?")
         ) from exc
     execute_from_command_line(sys.argv)
--- mysite/urls.py
+++ mysite/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-
+#Comment
 urlpatterns = [
     path('admin/', admin.site.urls),
 	path('',include('blog.urls')),
